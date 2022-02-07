0598f9e4cc0d72f867412a293c82f1bb871f265f
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-02-07 13:00:01 -0300

    ✨ Create Cards components

---
+++ components/Cards/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { 
+  Container,
+  Box,
+  Icon,
+  Title,
+} from './styles';
+
+const Cards = () => {
+  return (
+    <Container>
+      <Box>
+        <Icon/>
+        <Title>Meus Cartōes</Title>
+      </Box>
+    </Container>
+  );
+}
+
+export default Cards;
